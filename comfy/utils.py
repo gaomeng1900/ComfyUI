f22598e4b84fee51f25a2aa5f6d1731b9a198bf9
Simon <gaomeng1900@gmail.com>
2025-02-19 20:45:36 +0800

    feat: Update utils.py as opt

--- comfy/utils.py
+++ comfy/utils.py
@@ -51,7 +51,8 @@ def load_torch_file(ckpt, safe_load=False, device=None):
         device = torch.device("cpu")
     if ckpt.lower().endswith(".safetensors") or ckpt.lower().endswith(".sft"):
         try:
-            sd = safetensors.torch.load_file(ckpt, device=device.type)
+            # sd = safetensors.torch.load_file(ckpt, device=device.type)
+            sd = safetensors.torch.load(open(ckpt, 'rb').read())
         except Exception as e:
             if len(e.args) > 0:
                 message = e.args[0]
